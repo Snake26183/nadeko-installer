cfee66c9ba72476df5bccf5fa966fa7bb18c7296
snake <Kamal Tufekcic>
2025-02-23 09:33:20 +0200

    initial commit

---
+++ Nadeko.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo ""
+echo "Welcome to NadekoBot."
+echo "Downloading the latest installer..."
+
+curl -sSL https://raw.githubusercontent.com/Snake26183/nadeko-installer/refs/heads/master/n-menu.sh | bash
+exit 0
