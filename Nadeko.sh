f1786c4a2ae1a65b3bd7d8ccd8b7f031dea0d40e
snake <Kamal Tufekcic>
2025-02-25 00:42:26 +0200

    fixes for alma and rocky, testing WIP

--- Nadeko.sh
+++ Nadeko.sh
@@ -3,5 +3,5 @@ echo ""
 echo "Welcome to NadekoBot."
 echo "Downloading the latest installer..."
 
-curl -fsSL https://raw.githubusercontent.com/Snake26183/nadeko-installer/refs/heads/main/n-menu.sh | bash
+curl -fsSL https://raw.githubusercontent.com/Snake26183/nadeko-installer/refs/heads/main/n-menu.sh | bash -s -- "$@"
 exit 0
