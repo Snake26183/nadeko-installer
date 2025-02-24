2ef58d6bde89a1f8fb96b6cc07dbc396b6a9348d
snake <Kamal Tufekcic>
2025-02-25 01:29:23 +0200

    this n that

--- Nadeko.sh
+++ Nadeko.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 echo ""
 echo "Welcome to NadekoBot."
 echo "Downloading the latest installer..."
--- n-menu.sh
+++ n-menu.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Distro check and prep
-source <(curl -fsSL https://raw.githubusercontent.com/Snake26183/nadeko-installer/refs/heads/main/vars)
+source <(curl -fsSL https://raw.githubusercontent.com/Snake26183/nadeko-installer/refs/heads/main/vars 2>/dev/null)
 
 function run_nadeko {
     if [[ ! -f nadeko/data/creds.yml ]]; then
